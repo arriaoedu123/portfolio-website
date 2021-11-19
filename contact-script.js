b9491a960c2816b64a21e7f4708828b993b0bdba
Aryon Rabello <73148019+arriaoedu123@users.noreply.github.com>
2021-11-19 01:27:50 -0300

    Update files

--- contact-script.js
+++ contact-script.js
@@ -17,34 +17,23 @@
     })
 })()
 
-let sidebar = document.querySelector(".sidebar")
-let menuBtn = document.querySelector(".menu-btn")
-let body = document.querySelector("body")
-let toggleMode = document.querySelector("#themeButton")
-
-
+const sidebar = document.querySelector(".sidebar")
+const menuBtn = document.querySelector(".menu-btn")
+const body = document.querySelector("body")
+const toggleMode = document.querySelector("#themeButton")
+const textarea = document.querySelector(".textarea")
 
 menuBtn.addEventListener("click", () => {
+
   sidebar.classList.toggle("open")
   menuBtn.classList.toggle("open")
+
 })
 
-if (localStorage.getItem("ClassName") !== null) {
-  $("body").addClass(localStorage.getItem('ClassName'))
-} else {
-  $("body").addClass("dark")
-}
+textarea.addEventListener("keyup", e => {
 
-$("#themeButton").on("click",function() {
-  if ($("body").hasClass('dark')) {
+  textarea.style.height = "86px"
+  let areaHeight = e.target.scrollHeight
+  textarea.style.height = `${areaHeight}px`
 
-    $("body").removeClass("dark").addClass("light")
-    localStorage.setItem('ClassName', 'light')
-
-  } else {
-
-    $("body").removeClass("light").addClass("dark")
-    localStorage.setItem('ClassName', 'dark')
-
-  }
 })
